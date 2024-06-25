afe5c21b5aac6f905ab3c70105814206da75d43b
yooshnn <yooshnn@kakao.com>
2024-06-25 13:27:30 +0900

    [SBL-3] NextJS 초기 세팅 진행

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: '설문이용',
+  description: '설문 작성부터 홍보까지 한 방에!',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello world!</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </main>
+  );
+}
