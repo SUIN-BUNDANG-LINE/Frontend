e2a2244691f7baf5c4a5f986294696f0a092d729
yooshnn <yooshnn@kakao.com>
2024-07-03 16:38:02 +0900

    [SBL-3] eslint, prettier 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <main>
       <h1>Hello world!</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua.
       </p>
     </main>
   );
