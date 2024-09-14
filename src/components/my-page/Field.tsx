d7973871f821725db845494c696db79501f8ba3f
JeongHunHui <deca3248@gmail.com>
2024-09-15 06:21:05 +0900

    [SBL-128] 마이페이지의 설문 목록을 보여주기 위한 Table, Field 컴포넌트 구현, 유저 정보를 보여주는 헤더 컴포넌트 구현

---
+++ src/components/my-page/Field.tsx
@@ -0,0 +1,21 @@
+import Wrapper from '@/components/layout/Wrapper';
+import SurveyCreateButton from '@/components/my-page/SurveyCreateButton';
+import styles from './Field.module.css';
+
+interface Props {
+  title: string;
+}
+
+export default function Field({ title, children }: React.PropsWithChildren<Props>) {
+  return (
+    <Wrapper>
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <span style={{ marginLeft: '5px' }}>{title}</span>
+          <SurveyCreateButton />
+        </div>
+        <div className={styles.content}>{children}</div>
+      </div>
+    </Wrapper>
+  );
+}
