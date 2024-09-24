ac0f246e73e2fe634053f6fc5d3edee0165a36ef
JeongHunHui <deca3248@gmail.com>
2024-09-25 01:20:09 +0900

    [SBL-124] workbench 페이지와 질문 유형 icon 통일

--- src/components/management/result/QuestionResultViewer.tsx
+++ src/components/management/result/QuestionResultViewer.tsx
@@ -1,16 +1,32 @@
 import { QuestionResult } from '@/services/result/types';
-import { FaAlignJustify, FaCheckSquare, FaFont, FaUsers } from 'react-icons/fa';
+import { FaUsers } from 'react-icons/fa';
 import styles from './QuestionResultViewer.module.css';
 import PieChartComponent from './PieChartComponent';
 import TextResponseList from './TextResponseList';
+import Svg from '../misc/Svg';
 
 export default function QuestionResultViewer({ questionResult }: { questionResult: QuestionResult }) {
   const { title, responses, participantCount, type } = questionResult;
 
+  const fields = [
+    {
+      path: 'M480-280q83 0 141.5-58.5T680-480q0-83-58.5-141.5T480-680q-83 0-141.5 58.5T280-480q0 83 58.5 141.5T480-280Zm0 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z',
+      id: 'tool-radio',
+    },
+    {
+      path: 'm424-312 282-282-56-56-226 226-114-114-56 56 170 170ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z',
+      id: 'tool-checkbox',
+    },
+    {
+      path: 'M280-160v-520H80v-120h520v120H400v520H280Zm360 0v-320H520v-120h360v120H760v320H640Z',
+      id: 'tool-text',
+    },
+  ];
+
   const questionTypeMap: { [key: string]: { label: string; icon: JSX.Element } } = {
-    SINGLE_CHOICE: { label: '단일 선택', icon: <FaCheckSquare color="#5b5b5b" /> },
-    MULTIPLE_CHOICE: { label: '다중 선택', icon: <FaAlignJustify color="#5b5b5b" /> },
-    TEXT_RESPONSE: { label: '주관식', icon: <FaFont color="#5b5b5b" /> },
+    SINGLE_CHOICE: { label: '단일 선택', icon: <Svg path={fields[0].path} size="15px" /> },
+    MULTIPLE_CHOICE: { label: '다중 선택', icon: <Svg path={fields[1].path} size="15px" /> },
+    TEXT_RESPONSE: { label: '주관식', icon: <Svg path={fields[2].path} size="15px" /> },
   };
 
   return (
