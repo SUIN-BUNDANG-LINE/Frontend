6fac97603a9e9366461e6904c795460b2cc810b6
JeongHunHui <deca3248@gmail.com>
2024-10-03 02:03:53 +0900

    [SBL-154] 핑거프린트 설정 오타 수정

--- src/providers/FpjsProvider.tsx
+++ src/providers/FpjsProvider.tsx
@@ -3,7 +3,7 @@
 import { FpjsProvider as Provider, FingerprintJSPro } from '@fingerprintjs/fingerprintjs-pro-react';
 
 export default function FpjsProvider({ children }: React.PropsWithChildren) {
-  const apiKey = process.env.NEXT_PUBLIC_API_KEY!;
+  const apiKey = process.env.NEXT_PUBLIC_CREDENTIAL_API_KEY!;
   const credentialServerUrl = process.env.NEXT_PUBLIC_CREDENTIAL_SERVER_URL;
 
   return (
