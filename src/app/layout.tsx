c24933d49aa90dcaa2b15b7c459b64e3ebb48fa3
JeongHunHui <deca3248@gmail.com>
2024-10-14 03:02:24 +0900

    [SBL-191] Layout에서 jwt 인증 로직 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { cookies } from 'next/headers';
 import { GoogleAnalytics } from '@next/third-parties/google';
 
-import { decodeBase64 } from '@/utils/misc';
+import jwt from 'jsonwebtoken';
 import Providers from '@/providers/Providers';
 import type { User } from '@/providers/auth/types';
 
@@ -15,19 +15,60 @@ export const metadata: Metadata = {
   icons: { icon: '/assets/favicon.ico' },
 };
 
+function decodeBase64URL(value: string): Buffer {
+  let base64String = value.replace(/-/g, '+').replace(/_/g, '/');
+
+  const padding = base64String.length % 4;
+  if (padding === 2) {
+    base64String += '==';
+  } else if (padding === 3) {
+    base64String += '=';
+  }
+
+  return Buffer.from(base64String, 'base64');
+}
+
+const secret = decodeBase64URL(`${process.env.JWT_SECRET}`);
+
+function getPayload(token: string | undefined): jwt.JwtPayload | undefined {
+  if (!token) return undefined;
+  try {
+    const decodedToken = jwt.verify(token, secret);
+    if (typeof decodedToken === 'object') return decodedToken;
+  } catch {
+    return undefined;
+  }
+  return undefined;
+}
+
 export default async function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
   const cookieStore = cookies();
   let user: undefined | User;
 
-  if (cookieStore.has('user-profile') && cookieStore.has('JSESSIONID')) {
+  const accessTokenString = cookieStore.get('access-token')?.value;
+  const accessToken = getPayload(accessTokenString);
+  const refreshTokenString = cookieStore.get('refresh-token')?.value;
+  const refreshToken = getPayload(refreshTokenString);
+
+  // 엑세스 토큰과 리프레시 토큰이 유효하면 통과
+  if (accessToken && refreshToken && accessToken.sub && accessToken.nickname) {
+    user = { id: accessToken.sub, nickname: accessToken.nickname };
+  }
+  // 리프레시 토큰은 유효한데 엑세스 토큰이 유효하지 않은 경우
+  else if (!accessToken && refreshToken) {
     try {
-      const { value } = cookieStore.get('user-profile')!;
-      const data = JSON.parse(decodeBase64(value));
-      if (typeof data !== 'object') throw new Error();
-      if (!data.nickname || typeof data.nickname !== 'string') throw new Error();
-      if (!data.id || typeof data.id !== 'string') throw new Error();
-      user = { nickname: data.nickname, id: data.id } as User;
-    } catch (e) {
+      // 유저 프로필 API 호출해서 엑세스 토큰 재발급
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/user/profile`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Cookie: `refresh-token=${refreshTokenString}`,
+        },
+        credentials: 'include',
+      });
+      const userProfile = await response.json();
+      if (userProfile.id && userProfile.nickname) user = userProfile;
+    } catch {
       user = undefined;
     }
   }
