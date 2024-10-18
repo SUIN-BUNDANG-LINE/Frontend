06e10b4ed47cc788fc88502a1ac03a02cb5f8601
JeongHunHui <deca3248@gmail.com>
2024-10-19 03:59:21 +0900

    [SBL-210] 정렬 옵션 추가, 필터 드롭다운과 옵션 추가

--- src/components/main/survey-finder/SurveyFinder.tsx
+++ src/components/main/survey-finder/SurveyFinder.tsx
@@ -12,7 +12,10 @@ import Customize from './control/Customize';
 import Pagination from './control/Pagination';
 import styles from './SurveyFinder.module.css';
 
-const SORT_OPTIONS = [{ value: 'RECENT', label: '최신순' }];
+const SORT_OPTIONS = [
+  { value: 'RECENT', label: '최신순' },
+  { value: 'OLDEST', label: '오래된순' },
+];
 
 const getSortFromSearchParams = (searchParams: ReadonlyURLSearchParams) => {
   const arg = searchParams.get('sort');
@@ -20,19 +23,41 @@ const getSortFromSearchParams = (searchParams: ReadonlyURLSearchParams) => {
   return tar ? tar.value : SORT_OPTIONS[0].value;
 };
 
+const FILTER_OPTIONS = [
+  { value: [undefined, undefined], label: '전체' },
+  { value: [true, undefined], label: '리워드 O' },
+  { value: [undefined, true], label: '통계 공개' },
+  { value: [true, true], label: '리워드 O, 통계 공개' },
+];
+
+const getFilterFromSearchParams = (searchParams: ReadonlyURLSearchParams) => {
+  const rewardArg = searchParams.get('reward') ? Boolean(searchParams.get('reward')) : undefined;
+  const resultOpenArg = searchParams.get('resultOpen') ? Boolean(searchParams.get('resultOpen')) : undefined;
+  const tar = FILTER_OPTIONS.find((option) => option.value[0] === rewardArg && option.value[1] === resultOpenArg);
+  return tar ? tar.value : FILTER_OPTIONS[0].value;
+};
+
 export default function SurveyFinder() {
   const searchParams = useSearchParams();
 
   const [sort, setSort] = useState<string>(getSortFromSearchParams(searchParams));
+  const [filter, setFilter] = useState<(boolean | undefined)[]>(getFilterFromSearchParams(searchParams));
   const [page, setPage] = useState(Number(searchParams.get('page')) || 1);
 
-  const { data, isLoading, isError, refetch } = useSurveysList(sort, page);
+  const { data, isLoading, isError, refetch } = useSurveysList(sort, page, filter[0], filter[1]);
 
   const { pageCount, surveys } = data || { pageCount: 1, surveys: [] };
 
   const setSortHandler = (value: string) => {
     setSort(value);
     replaceURLSearchParams('sort', value);
+  };
+
+  const setFilterHandler = (value: (boolean | undefined)[]) => {
+    setFilter(value);
+    const [reward, resultOpen] = value;
+    if (reward) replaceURLSearchParams('reward', 'true');
+    if (resultOpen) replaceURLSearchParams('resultOpen', 'true');
   };
 
   const setPageHandler = (value: number) => {
@@ -63,7 +88,14 @@ export default function SurveyFinder() {
 
   return (
     <>
-      <Customize sort={sort} setSortHandler={setSortHandler} sortOptions={SORT_OPTIONS} />
+      <Customize
+        sort={sort}
+        setSortHandler={setSortHandler}
+        sortOptions={SORT_OPTIONS}
+        filter={filter}
+        setFilterHandler={setFilterHandler}
+        filterOptions={FILTER_OPTIONS}
+      />
       {content}
       <Pagination page={page} setPageHandler={setPageHandler} maxPage={pageCount} />
     </>
--- src/components/main/survey-finder/control/Customize.tsx
+++ src/components/main/survey-finder/control/Customize.tsx
@@ -12,11 +12,26 @@ interface Props {
     value: string;
     label: string;
   }[];
+  filter: (boolean | undefined)[];
+  setFilterHandler: (value: (boolean | undefined)[]) => void;
+  filterOptions: {
+    value: (boolean | undefined)[];
+    label: string;
+  }[];
 }
 
-export default function Customize({ sort, setSortHandler, sortOptions }: Props) {
+export default function Customize({
+  sort,
+  setSortHandler,
+  sortOptions,
+  filter,
+  setFilterHandler,
+  filterOptions,
+}: Props) {
   const { isOpen, fn, dropdownRef } = useDropdown();
+  const { isOpen: filterIsOpen, fn: filterFn, dropdownRef: filterDropdownRef } = useDropdown();
 
+  // 정렬 드롭다운 고정 콘텐츠
   const fixedContent = (
     <button type="button" className={styles.selector}>
       <span>{sortOptions.find((i) => i.value === sort)?.label}</span>
@@ -24,6 +39,14 @@ export default function Customize({ sort, setSortHandler, sortOptions }: Props) 
     </button>
   );
 
+  const filterFixedContent = (
+    <button type="button" className={styles.selector}>
+      <span>{filterOptions.find((i) => i.value === filter)?.label}</span>
+      <FaAngleDown className={`${styles.angle} ${isOpen ? styles.active : undefined}`} />
+    </button>
+  );
+
+  // 정렬 옵션 콘텐츠
   const toggleContent = sortOptions.map(({ value, label }) => {
     return (
       <Content key={value} clickHandler={() => setSortHandler(value)}>
@@ -32,9 +55,19 @@ export default function Customize({ sort, setSortHandler, sortOptions }: Props) 
     );
   });
 
+  // 필터 옵션 콘텐츠
+  const filterToggleContent = filterOptions.map(({ value, label }) => {
+    return (
+      <Content key={label} clickHandler={() => setFilterHandler(value)}>
+        <div className={styles.selectorItem}>{label}</div>
+      </Content>
+    );
+  });
+
   return (
     <Wrapper outerColor="#fff" zIndex={1}>
       <div className={styles.customize}>
+        {/* 정렬 드롭다운 */}
         <div className={styles.sort}>
           <Dropdown
             fixedContent={fixedContent}
@@ -42,6 +75,18 @@ export default function Customize({ sort, setSortHandler, sortOptions }: Props) 
             fn={fn}
             isOpen={isOpen}
             dropdownRef={dropdownRef}
+            style={{ width: '172px', fontSize: '18px' }}
+          />
+        </div>
+
+        {/* 필터 드롭다운 */}
+        <div className={styles.filter}>
+          <Dropdown
+            fixedContent={filterFixedContent}
+            toggleContent={filterToggleContent}
+            fn={filterFn}
+            isOpen={filterIsOpen}
+            dropdownRef={filterDropdownRef}
             style={{ width: '172px', fontSize: '18px' }}
           />
         </div>
