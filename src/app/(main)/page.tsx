6542f205b9bc1787f28ddd1eb20ded5e0b5c3fa9
yooshnn <yooshnn@kakao.com>
2024-10-26 23:44:29 +0900

    heading 추가

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,14 +1,16 @@
 'use client';
 
-import Banner from '@/components/main/banner/Banner';
-import { SurveyFinder } from '@/components/main';
+// import Banner from '@/components/main/banner/Banner';
+// import { SurveyFinder } from '@/components/main';
 import { Suspense } from 'react';
+import Heading from '@/components/main/heading';
 
 export default function Page() {
   return (
     <Suspense>
-      <Banner />
-      <SurveyFinder />
+      <Heading />
+      {/* <Banner /> */}
+      {/* <SurveyFinder /> */}
     </Suspense>
   );
 }
