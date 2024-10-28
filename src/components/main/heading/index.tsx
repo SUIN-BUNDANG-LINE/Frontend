b32dc1089e77ee8cba169c4fb44cde00e9aebeec
yooshnn <yooshnn@kakao.com>
2024-10-28 19:19:41 +0900

    로그인 상태에 따라 로그인 버튼을 마이페이지 버튼으로 변경

--- src/components/main/heading/index.tsx
+++ src/components/main/heading/index.tsx
@@ -1,7 +1,10 @@
 import Link from 'next/link';
+import { useAuth } from '@/hooks/useAuth';
 import styles from './index.module.css';
 
 export default function Heading() {
+  const guest = !useAuth().user;
+
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.title}>
@@ -9,9 +12,16 @@ export default function Heading() {
       </h1>
       <p className={styles.subtitle}>1분이면 충분합니다. 지금 바로 설문조사를 만들어보세요!</p>
       <div className={styles.links}>
-        <Link href="/login">
-          <div className={styles.login}>소셜 로그인으로 시작하기</div>
-        </Link>
+        {guest && (
+          <Link href="/login">
+            <div className={styles.login}>소셜 로그인으로 시작하기</div>
+          </Link>
+        )}
+        {!guest && (
+          <Link href="/mypage">
+            <div className={styles.login}>설문조사 만들러 가기</div>
+          </Link>
+        )}
         <Link href="/s">
           <div className={styles.surveys}>공개된 설문조사 둘러보기 →</div>
         </Link>
