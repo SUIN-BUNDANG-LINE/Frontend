7dfc63e2baed5c0bfe194e7216c0313963fd81e3
JeongHunHui <deca3248@gmail.com>
2024-12-18 15:33:47 +0900

    cicd 비활성화(vercel 사용 예정)

--- .github/workflows/prod_deploy.sh
+++ .github/workflows/prod_deploy.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
-# ubuntu 권한으로 모든 명령어 실행
-sudo -u ubuntu sh <<EOF
-cd ~/Frontend
-git checkout main
-git pull
-echo "$ENV_CONTENT" | base64 -d > .env.local
-npm install
-npm run build
-pm2 start pm2.config.js
-EOF
+# #!/bin/sh
+# # ubuntu 권한으로 모든 명령어 실행
+# sudo -u ubuntu sh <<EOF
+# cd ~/Frontend
+# git checkout main
+# git pull
+# echo "$ENV_CONTENT" | base64 -d > .env.local
+# npm install
+# npm run build
+# pm2 start pm2.config.js
+# EOF
