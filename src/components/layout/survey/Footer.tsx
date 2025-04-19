147ce7900cfa3d4180809e4cdebaa8879f6e4fa6
JeongHunHui <deca3248@gmail.com>
2025-04-19 14:06:27 +0900

    HOTFIX: 메인 페이지 말고 다른 Footer도 메인 페이지와 통일

--- src/components/layout/survey/Footer.tsx
+++ src/components/layout/survey/Footer.tsx
@@ -1,12 +1,25 @@
+/* eslint-disable no-irregular-whitespace */
 import Link from 'next/link';
 import styles from './Footer.module.css';
 
 export default function Footer() {
   return (
-    <div className={styles.wrapper}>
+    <footer className={styles.wrapper}>
       <div className={styles.container}>
-        powered by <Link href="/">설문이용</Link>
+        {/* 브랜드 / 서비스명 */}
+        <h2 className={styles.brand}>
+          <Link href="/">설문이용</Link>
+        </h2>
+
+        <nav className={styles.policy}>
+          <Link href="mailto:sulmoon.io@gmail.com">제휴 및 파트너십 문의&nbsp;sulmoon.io@gmail.com</Link>
+          <span className={styles.delimiter}>|</span>
+          <Link href="/s/ba73cc3f-8c54-4fa9-9a31-6d92c27e0c50">문의 및 건의 사항</Link>
+        </nav>
+
+        {/* 카피라이트 */}
+        <p className={styles.copyright}>© {new Date().getFullYear()} 설문이용. All rights reserved.</p>
       </div>
-    </div>
+    </footer>
   );
 }
